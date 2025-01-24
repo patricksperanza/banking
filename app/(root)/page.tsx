562d2df35aeae479aeeb5a38a92f46f00787fdde
Patrick Speranza <patrick.m.speranza@gmail.com>
2025-01-23 20:52:06 -0600

    root main content

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,27 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 const Home = () => {
-  return <div>page</div>;
+  const loggedIn = { firstName: "Patrick" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
