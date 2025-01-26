17c3e37d90f36f0a8791ac30f413f61fd52cdbc0
Patrick Speranza <patrick.m.speranza@gmail.com>
2025-01-25 21:05:08 -0600

    mobile nav

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,7 +12,13 @@ const Sidebar = ({ user }: SidebarProps) => {
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
         <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
-          <Image src="/icons/logo.svg" width={34} height={34} alt="Bank logo" />
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Bank logo"
+            className="size-[24px] max-xl:size-14"
+          />
           <h1 className="sidebar-logo">Horizon</h1>
         </Link>
         {sidebarLinks.map((item) => {
